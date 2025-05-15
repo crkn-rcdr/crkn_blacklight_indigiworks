99a63e77203f05de711fe16961b2d63a7c81b47d
Brittny Lapierre <brittny.lapierre@gmail.com>
2025-05-15 15:02:07 -0400

    Rendering options for mirador

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -27,24 +27,145 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
 //import "../javascript/application"
+let pageViewer = document.getElementById("my-mirador")
+if(pageViewer) {
 
-console.log("Mirador", Mirador)
-var miradorViewer = Mirador.viewer({
-  "id": "my-mirador",
-  "manifests": {
-    "https://iiif.lib.harvard.edu/manifests/drs:48309543": {
-      "provider": "Harvard University"
-    }
-  },
-  "windows": [
-    {
-      "loadedManifest": "https://iiif.lib.harvard.edu/manifests/drs:48309543",
-      "canvasIndex": 2,
-      "thumbnailNavigationPosition": 'far-bottom'
-    }
-  ]
-});
-console.log("miradorViewer", miradorViewer)
+    const documentId = "oocihm.9_07160";//pageViewer.getAttribute("data-docid")
+    let canvasIndex = 0
+    const params = new URLSearchParams(window.location.search)
+    if(params.has("pageNum")) canvasIndex = parseInt(params.get("pageNum")-1)
+
+    let apiPrefix =  "https://www.canadiana.ca/iiif/"
+    let manifest = apiPrefix+documentId+"/manifest"
+    const manifestList = {} 
+    manifestList[manifest] = { "provider": "Canadian Research Knowledge Network" }
+    console.log("Mirador", Mirador)
+    var miradorViewer = Mirador.viewer({
+        id: "my-mirador",
+        manifests: manifestList,
+        windows: [
+        {
+            manifestId: manifest,
+            //view: 'single',
+            canvasIndex: canvasIndex,
+        }],
+
+        view: "catalogueView",
+        window: {
+
+            //imageToolsOpen: false,
+    
+            //global window defaults
+    
+            allowClose: false, // Configure if windows can be closed or not
+    
+            allowFullscreen: false, // Configure to show a "fullscreen" button in the WindowTopBar
+    
+            allowMaximize: false, // Configure if windows can be maximized or not
+    
+            allowTopMenuButton: false, // Configure if window view and thumbnail display menu are visible or not
+    
+            allowWindowSideBar: false, // Configure if side bar menu is visible or not
+    
+            authNewWindowCenter: "parent", // Configure how to center a new window created by the authentication flow. Options: parent, screen
+    
+            sideBarPanel: "info", // Configure which sidebar is selected by default. Options: info, attribution, canvas, annotations, search
+    
+            defaultSidebarPanelHeight: 201, // Configure default sidebar height in pixels
+    
+            defaultSidebarPanelWidth: 235, // Configure default sidebar width in pixels
+    
+            defaultView: "single", // Configure which viewing mode (e.g. single, book, gallery) for windows to be opened in
+    
+            forceDrawAnnotations: false,
+    
+            hideWindowTitle: true, // Configure if the window title is shown in the window title bar or not
+    
+            highlightAllAnnotations: false, // Configure whether to display annotations on the canvas by default
+    
+            showLocalePicker: false, // Configure locale picker for multi-lingual metadata
+    
+            sideBarOpen:  false, // Configure if the sidebar (and its content panel) is open by default
+    
+            switchCanvasOnSearch: true, // Configure if Mirador should automatically switch to the canvas of the first search result
+    
+            panels: {
+    
+              // Configure which panels are visible in WindowSideBarButtons
+    
+              info: false,
+    
+              attribution: false,
+    
+              canvas: false,
+    
+              annotations: false,
+    
+              search: false,
+    
+              layers: false
+    
+            },
+    
+            views: [
+    
+              { key: "single", behaviors: ["individuals"] },
+    
+              { key: "book", behaviors: ["paged"] },
+    
+              { key: "scroll", behaviors: ["continuous"] }
+    
+            ],
+    
+            elastic: {
+    
+              height: 400,
+    
+              width: 480
+    
+            }
+    
+          },
+    
+          
+          workspace: {
+    
+            draggingEnabled: false,
+    
+            allowNewWindows: true,
+    
+            isWorkspaceAddVisible: false, // Catalog/Workspace add window feature visible by default
+    
+            exposeModeOn: false, // unused?
+    
+            height: 5000, // height of the elastic mode's virtual canvas
+    
+            showZoomControls: true, // Configure if zoom controls should be displayed by default
+    
+            type: "mosaic", // Which workspace type to load by default. Other possible values are "elastic". If "mosaic" or "elastic" are not selected no worksapce type will be used.
+    
+            viewportPosition: {
+    
+              // center coordinates for the elastic mode workspace
+    
+              x: 0,
+    
+              y: 0
+    
+            },
+    
+            width: 5000 // width of the elastic mode's virtual canvas
+    
+          },
+    
+          workspaceControlPanel: {
+    
+            enabled: false // Configure if the control panel should be rendered.  Useful if you want to lock the viewer down to only the configured manifests
+    
+          },
+    });
+    console.log("miradorViewer", miradorViewer)
+}
 
 import BlacklightRangeLimit from 'blacklight-range-limit';
 //Blacklight.onLoad(() => {});
