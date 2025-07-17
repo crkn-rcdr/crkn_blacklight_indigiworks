1d1aaba4913797b753e7fdc6e72281243c1c66fe
Brittny Lapierre <brittny.lapierre@gmail.com>
2025-07-17 08:43:57 -0400

    Adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
   mount Blacklight::Engine => '/'
+  root to: "pages#home"
+  #root to: "catalog#index"
   concern :marc_viewable, Blacklight::Marc::Routes::MarcViewable.new
-  root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
   resource :catalog, only: [], as: 'catalog', path: '/catalog', controller: 'catalog', id: /[^\/]+/ do
